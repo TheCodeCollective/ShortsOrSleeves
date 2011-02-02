205fa4bbd17517bff73ea5c35dd5a8cadd0c1254
Evan <thecodecollective@googlegroups.com>
2011-02-02 10:16:12 -0500

    stylesheet first commit

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<?php
+$pages = array();
 
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>untitled</title>
-	<meta name="generator" content="TextMate http://macromates.com/">
-	<meta name="author" content="Evan">
-	<!-- Date: 2011-02-01 -->
-</head>
-<body>
+$curPage = 'home';
+if(isset($_GET['page']) && array_key_exists($_GET['page'],$pages))
+	$curPage = $_GET['page'];
+$mainContentFile = 'default.php';
 
-</body>
+if(array_key_exists($curPage,$pages)) {
+	$mainContentFile = $pages[$curPage];
+}
+
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link type='text/css' rel='stylesheet' href='style.css' />
+        <title>Shorts Or Sleeves</title>
+    </head>
+    <body>
+		<div id='container'>
+			
+	
+	
+	</body>
 </html>
+
